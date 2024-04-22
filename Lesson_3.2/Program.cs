1f0d64a0c5d35ac9ea968ae064062fa7c662fa4e
denis <miroshnichenkodenis2004@gmail.com>
2024-04-22 14:36:42 +0300

    complete lesson about scheduler, build custom schedulers and get some information about external and subsidiary tasks

---
+++ Lesson_3.2/Program.cs
@@ -0,0 +1,93 @@
+﻿var scheduler = new StackTaskScheduler();
+
+List<Task> tasks = new();
+for (int i = 0; i < 40; ++i)
+{
+    tasks.Add(new Task(() =>
+    {
+        Thread.Sleep(2000);
+        Console.WriteLine($"Задача с номером {Task.CurrentId} выполнилась выполнилась в потоке {Thread.CurrentThread.ManagedThreadId}");
+    }));
+}
+
+for (int i = 0; i < 40; ++i)
+    tasks[i].Start(scheduler);
+
+Task.WaitAll(tasks.ToArray());
+
+class StackTaskScheduler : TaskScheduler
+{
+    private Stack<Task> stack = new();
+    
+    protected override IEnumerable<Task>? GetScheduledTasks()
+    {
+        lock (stack)
+        {
+            return stack;
+        }
+    }
+
+    protected override void QueueTask(Task task)
+    {
+        lock (stack)
+        {
+            stack.Push(task);
+        }
+
+        Console.WriteLine($"TASK ID: {task.Id}");
+        ThreadPool.QueueUserWorkItem(Execute, null);
+    }
+
+    private void Execute(object? state)
+    {
+        // Thread.Sleep(5000);
+        Task? topTask = null;
+
+        lock (stack)
+        {
+            if (stack.Count == 0)
+                return;
+
+            topTask = stack.Peek();
+            stack.Pop();
+        }
+        
+        if (topTask is null)
+            return;
+
+        Console.WriteLine($"Задача была поставлена на выполнение планировщиков: taskId={topTask.Id}");
+        TryExecuteTask(topTask);
+    }
+
+    protected override bool TryExecuteTaskInline(Task task, bool taskWasPreviouslyQueued)
+    {
+        Console.WriteLine($"Выполняем задачу синхронно: {task.Id}");
+        return TryExecuteTask(task);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Lesson_3.3/Program.cs
@@ -0,0 +1,38 @@
+﻿
+var scheduler = new DelayTaskScheduler();
+
+var task = new Task(() =>
+    {
+        Console.WriteLine($"\nЗадача ыполнилась выполнилась в потоке {Thread.CurrentThread.ManagedThreadId} из пула потоков - {Thread.CurrentThread.IsThreadPoolThread}");
+    });
+
+task.Start(scheduler);
+
+while (!task.IsCompleted)
+{
+    Console.Write($"* ");
+    Thread.Sleep(100);
+}
+
+class DelayTaskScheduler : TaskScheduler
+{
+    protected override IEnumerable<Task>? GetScheduledTasks()
+    {
+        return Enumerable.Empty<Task>();
+    }
+
+    protected override void QueueTask(Task task)
+    {
+        Timer timer = new Timer(DelayCallBack, task, 2000, 0);
+    }
+
+    private void DelayCallBack(object? state)
+    {
+        TryExecuteTask((Task)state!);
+    }
+
+    protected override bool TryExecuteTaskInline(Task task, bool taskWasPreviouslyQueued)
+    {
+        return false;
+    }
+}
