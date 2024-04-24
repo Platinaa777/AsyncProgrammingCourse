9667f89e5f4327b8b4078b8dd6ef92b4f8fe353f
denis <miroshnichenkodenis2004@gmail.com>
2024-04-24 23:21:50 +0300

    complete 4 lesson about async/await structure and state machine which is built while async/await uses

---
+++ Lesson_4.1/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+var task = CalculateFactorialAsync(12);
+
+while (!task.IsCompleted)
+{
+    Console.Write('#');
+}
+
+int CalculateFactorial(int x)
+{
+    int multiply = 1;
+    while (x > 0)
+    {
+        multiply *= x;
+        x -= 1;
+    }
+
+    return multiply;
+}
+
+async Task CalculateFactorialAsync(int x)
+{
+    var result = await Task.Run(() => CalculateFactorial(x));
+
+    Console.WriteLine(result);
+}
+
+
+
+
+
+
+
+
+
+
---
+++ Lesson_4.2/Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+
+
+
+var test = File.ReadAllText("../../../input.txt");
+
+var task = ParseAsync(test);
+
+Console.WriteLine("Введите свое имя: ");
+var name = Console.ReadLine();
+
+await using FileStream fileStream = new FileStream("../../../out.txt", FileMode.Append);
+
+// Создание экземпляра класса StreamWriter на основе FileStream
+await using StreamWriter streamWriter = new StreamWriter(fileStream);
+
+var parseResult = await task;
+
+streamWriter.Write($"{name} нашел {parseResult.Count} уникальных слов.\n");
+
+for (int i = 0; i < parseResult.Count - 1; ++i)
+    streamWriter.Write(parseResult[i] + ", ");
+
+streamWriter.Write(parseResult[^1] + ".");
+
+async Task<IList<string>> ParseAsync(string inputData)
+{
+    var words = await Task.Run(() => inputData.Split(new[] { ' ', ',', '.' }).Distinct().ToList());
+
+    return words;
+}
+
+
+
+
+
+
