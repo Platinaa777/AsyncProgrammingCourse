ccbf9af254615184be74cb4d625a5d63a30afdb7
denis <miroshnichenkodenis2004@gmail.com>
2024-04-27 13:41:04 +0300

    complete 7 lesson, learn about async exception and how to handle it, how to cancel properly in async code, how to use progress in async code

---
+++ Lesson_7.1/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Lesson_7._1
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine($"Id основного потока: {Thread.CurrentThread.ManagedThreadId}");
+
+            Thread thread = new Thread(DrawSymbols);
+            thread.Start();
+
+            Thread.Sleep(500);
+
+            while (true)
+            {
+                Console.Write($" {Thread.CurrentThread.ManagedThreadId} ");
+                Thread.Sleep(350);
+            }
+        }
+
+        private static void DrawSymbols()
+        {
+            Console.WriteLine($"Id вторичного потока: {Thread.CurrentThread.ManagedThreadId}");
+            Thread.Sleep(500);
+
+            int loopCounter = 0;
+
+            while (true)
+            {
+                Console.Write($" {Thread.CurrentThread.ManagedThreadId} ");
+                Thread.Sleep(350);
+
+                try
+                {
+                    if (loopCounter++ == 12)
+                    {
+                        throw new Exception("Превышено количество итераций!");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Exception was handled");
+                    break;
+                }
+                
+            }
+        }
+    }
+}
---
+++ Lesson_7.2/Program.cs
@@ -0,0 +1,54 @@
+﻿
+namespace Lesson_7._2
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Task task = Task.Run(Operation);
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Метод Main работает в основном потоке.");
+                Thread.Sleep(400);
+            }
+
+            if (task.IsFaulted)
+            {
+                Console.WriteLine($"В задаче {task.Id} произошло исключение {task.Exception?.InnerExceptions[0].Message}");
+            }
+
+            Console.WriteLine($"\nПриложение корректно завершило свою работу!!!");
+            Console.ReadKey();
+        }
+
+        private static void Operation()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"        Задача работает во вторичном потоке.");
+                Thread.Sleep(400);
+
+                if(i == 5)
+                {
+                    throw new Exception("Превышено количество итераций!");
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
