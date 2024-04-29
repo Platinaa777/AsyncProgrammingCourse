524f92c368d26757d9c72defbcd0431909f25e88
denis <miroshnichenkodenis2004@gmail.com>
2024-04-29 15:38:34 +0300

    finish course, learn about concurrent dictionary and PLINQ

---
+++ Lesson_9.1/Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+
+
+
+
+var range = Enumerable.Range(1, 10_000_000);
+
+var only2Degree = range
+    .AsParallel()
+    .Where(x => (x & (x - 1)) == 0)
+    .ToList();
+
+foreach (var item in only2Degree)
+{
+    Console.Write(item + ", ");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Lesson_9.2/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+var wordsWhichStartFromA = File.ReadAllText("../../../data.txt")
+        .Split()                                                                                
+        .AsParallel()                                                                                                                                                                                                                   
+        .Where(word => word.StartsWith("a") || word.StartsWith("A"));
+
+foreach (var w in wordsWhichStartFromA)
+{
+        Console.WriteLine(w);
+}
+
+
+
+
+
